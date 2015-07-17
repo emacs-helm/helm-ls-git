f321f5352c3ca4b0678cdead5716945290e15d4c
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-07-17 07:00:47 +0200

    Fix defmethod with new source name for buffers.
    * helm-ls-git.el: Do it.

--- helm-ls-git.el
+++ helm-ls-git.el
@@ -400,7 +400,7 @@ The color of matched items can be customized in your .gitconfig."
 ;; Overhide the actions of helm-type-buffer.
 (defmethod helm--setup-source :after ((source helm-source-buffers))
   (let ((name (oref source :name)))
-    (when (string= name "Buffers in project")
+    (when (string= name "Buffers in git project")
       (set-slot-value
        source 'action
        (helm-append-at-nth
