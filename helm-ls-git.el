578b4c5f6f3ab53631ce1dc396f88f2e865e161b
Thierry Volpiatto <thievol@posteo.net>
2021-07-29 06:16:11 +0200

    Fix previous commit

--- helm-ls-git.el
+++ helm-ls-git.el
@@ -293,8 +293,8 @@ See docstring of `helm-ls-git-ls-switches'.
     (helm-force-update)))
 (put 'helm-ls-git-ls-switches 'helm-only t)
 
-(defun helm-ls-git-root-dir (&optional directory)
-  (locate-dominating-file directory (or directory default-directory)) ".git")
+(cl-defun helm-ls-git-root-dir (&optional (directory default-directory))
+  (locate-dominating-file directory ".git"))
 
 (defun helm-ls-git-not-inside-git-repo ()
   (not (helm-ls-git-root-dir)))
@@ -344,7 +344,7 @@ See docstring of `helm-ls-git-ls-switches'.
           (unless (zerop ret)
             (erase-buffer)
             (call-process "git" nil t nil "rev-parse" "--short" "HEAD")))
-        (buffer-substring-no-properties ((point-min))
+        (buffer-substring-no-properties (goto-char (point-min))
                                         (line-end-position)))))
 
 (defun helm-ls-git-header-name (name)
